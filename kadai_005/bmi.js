bc706cfca0556feaf74c72cc67ba93daab58ef19
hachitea <141470052+hachitea@users.noreply.github.com>
2023-11-02 19:42:38 +0900

    kadai_005を追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+//変数の宣言・値の代入
+let weight = 68;
+let height = 1.7;
+
+//コンソールへの出力
+console.log(weight);
+console.log(height);
+
+//BMIの計算式とコンソールへの出力
+let bmi = (weight / (height * height));
+console.log(bmi);
